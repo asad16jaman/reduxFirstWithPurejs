2fcc56654a0408b27451412c622aadcf965914b8
asad <asaduzzaman240250@gmail.com>
2023-09-20 12:24:35 +0600

    work with multiple state at a time

---
+++ multiple.js
@@ -0,0 +1,70 @@
+const {createStore, combineReducers} = require("redux")
+//initial state...
+let product = {
+    productName: ["sart","pant"],
+    numberOfProduct:2
+}
+
+let card = {
+    productName : ["sari"],
+    numberOfProduct: 1
+}
+
+
+//action function
+
+const addProduct = (val)=>{
+    return {
+        type:"ADD_P",
+        payload:val
+    }
+}
+
+const addCard = (val)=>{
+    return {
+        type:"ADD_C",
+        payload:val
+    }
+}
+
+const productReducer = (state=product,action)=>{
+    switch(action.type){
+        case "ADD_P":
+            return {
+                product:[...state.productName,action.payload],
+                numberOfProduct:state.numberOfProduct + 1
+            }
+        default :
+            return state
+    }
+}
+
+const cardReducer = (state=card,action)=>{
+    switch(action.type){
+        case "ADD_C":
+            return {
+                product:[...state.productName,action.payload],
+                numberOfProduct:state.numberOfProduct + 1
+            }
+        default :
+            return state
+    }
+}
+
+
+let alstate = combineReducers({
+    productReducer,
+    cardReducer
+})
+
+let store = createStore(alstate)
+
+
+
+store.subscribe(()=>{
+    console.log(store.getState().productReducer)
+})
+
+
+store.dispatch(addProduct("cap"))
+
